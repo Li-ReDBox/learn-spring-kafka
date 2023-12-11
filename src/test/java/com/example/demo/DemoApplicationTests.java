ebbd72cbaf55cef4b33c10cf61857a338ca5c13d
Jianfeng Li <jianfeng.li@pwc.com>
2023-12-11 16:47:43 +1030

    Check beans

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    void loadBeans() {
+        assertNotNull(applicationContext.getBean(KProducer.class));
+        assertNotNull(applicationContext.getBean(KConsumer.class));
+    }
 }
